21d9a7aa4e2d5334db9c390dc1a6675256004cbc
yangzhaooba@gmail.com <yangzhaooba@gmail.com>
2023-03-24 12:36:39 +0100

    repo for cog

--- run_emdqn.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python train_emdqn.py --wandb --env_name 'MinAtar/Freeway-v1' --sticky_action_prob 0.01 &
--- run_lander.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-python train_lunar_lander.py --ec_factor_start 0 --sampling_weight_start 0 --wandb &
-python train_lunar_lander.py --ec_factor_start 1 --sampling_weight_start 0 --wandb &
-
-python train_lunar_lander.py --ec_factor_start 0.9 --ec_factor_end 0.1 --sampling_weight_start 0.1 --sampling_weight_end 0.9 --wandb &
-python train_lunar_lander.py --ec_factor_start 0.5 --ec_factor_end 0.1 --sampling_weight_start 0.1 --sampling_weight_end 0.5 --wandb &
-python train_lunar_lander.py --ec_factor_start 0.9 --ec_factor_end 0.1 --sampling_weight_start 0.9 --sampling_weight_end 0.1 --wandb &
-python train_lunar_lander.py --ec_factor_start 0.9 --ec_factor_end 0.1 --sampling_weight_start 0.5 --sampling_weight_end 0.1 --wandb &
